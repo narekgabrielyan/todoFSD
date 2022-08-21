a92e086de6f5223e94456f9e17de4407bfcd4066
narekgabrielyan <narekgabrielyan998@gmail.com>
2022-08-21 16:19:44 +0400

    chore: app.tsx changed places with index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from '../App';
-import reportWebVitals from '../reportWebVitals';
+import React from "react";
+import logo from "../logo.svg";
+import "./index.scss";
+import { withProviders } from "./providers";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/Index.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default withProviders(App);
