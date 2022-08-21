693ababf36149bb46e11d7042d88b55f42830a61
narekgabrielyan <narekgabrielyan998@gmail.com>
2022-08-21 22:46:03 +0400

    chore: config added for environmental variables

---
+++ src/shared/config/index.ts
@@ -0,0 +1,11 @@
+const getEnvVar = (key: string) => {
+  if (process.env[key] === undefined) {
+    throw new Error(`Env variable ${key} is required`);
+  }
+  return process.env[key] || "";
+};
+
+export const API_URL = getEnvVar("REACT_APP_API_URL");
+export const NODE_ENV = getEnvVar("NODE_ENV");
+export const isDevEnv = NODE_ENV === "development";
+export const isProdEnv = NODE_ENV === "production";
