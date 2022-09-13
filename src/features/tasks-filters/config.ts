4c94149ef4a42d8648eea81a18794e5853d4fad9
narek.gabrielyan <narekgabrielyan998@gmail.com>
2022-09-14 01:24:14 +0400

    chore: task-filters components added

---
+++ src/features/tasks-filters/config.ts
@@ -0,0 +1,33 @@
+import { taskModel } from "entities/task";
+// Todo: Review and update if dynamic import is needed
+
+export type Filter = {
+    id: number;
+    title: string;
+    config: taskModel.QueryConfig;
+}
+
+// Описываем здесь датасет фильтров "Закрытые" / "Открытые" и т.п.
+export const filters: Record<number, Filter> = {
+    1: {
+        id: 1,
+        title: "All",
+        config: {},
+    },
+    2: {
+        id: 2,
+        title: "Opened",
+        config: { completed: false },
+    },
+    3: {
+        id: 3,
+        title: "Closed",
+        config: { completed: true },
+    },
+};
+
+export const DEFAULT_FILTER = 1;
+
+export const filtersList = Object.values(filters);
+// Todo: Check id on click
+export const getFilterById = (id: number) => filters[id];
